0eb67c34e23efb04e20750c8972625de66865729
ZouJian <chsword@126.com>
2015-12-05 23:44:33 +0800

    add bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp');
-
-gulp.task('default', function() {
+var path={
+  bower:"./bower_components/"
+};
+gulp.task('scripts', function() {
   // 将你的默认的任务代码放在这
-  gulp.src("./bower_components/**/*.min.js")
+  gulp.src(path.bower+"**/*.min.js")
   .pipe(gulp.dest("./scripts/"));
 });
+
+
+gulp.task('bootstrap', function() {
+  // 将你的默认的任务代码放在这
+  gulp.src(path.bower+"bootstrap/dist/css/*.css")
+  .pipe(gulp.dest("./content"));
+});
+
+gulp.task("default",["scripts","bootstrap"]);
