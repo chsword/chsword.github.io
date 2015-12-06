a7ca31df33851b9355ddcd5c94a839944c6477eb
ZouJian <chsword@126.com>
2015-12-06 18:26:53 +0800

    use angular2

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
 var gulp = require('gulp');
+var basePath="./wwwroot/"
 var path={
-  bower:"./bower_components/"
+  bower:"./bower_components/",
+  npm:"./node_modules/",
+  js:basePath+"scripts/",
+  "static":basePath+"content/"
+  
 };
 gulp.task('scripts', function() {
-  // 将你的默认的任务代码放在这
   gulp.src(path.bower+"**/*.min.js")
-  .pipe(gulp.dest("./scripts/"));
+  .pipe(gulp.dest(path.js));
 });
 
 
 gulp.task('bootstrap', function() {
-  // 将你的默认的任务代码放在这
   gulp.src(path.bower+"bootstrap/dist/css/*.css")
-  .pipe(gulp.dest("./content"));
+  .pipe(gulp.dest(path.static));
 });
-
-gulp.task("default",["scripts","bootstrap"]);
+gulp.task('angular', function() {
+  gulp.src(path.npm+"angular2/bundles/*.*")
+  .pipe(gulp.dest(path.js+"angular2/"));
+  gulp.src(path.bower+"systemjs/build/*.js")
+  .pipe(gulp.dest(path.js+"systemjs/"));
+   
+    gulp.src(path.bower+"es6-shim/dist/*.min.js")
+  .pipe(gulp.dest(path.js+"es6-shim/"));
+});
+gulp.task("default",["scripts","bootstrap","angular"]);
